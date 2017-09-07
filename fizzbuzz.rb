4718c291df813959b123ba5ddf52081d9eca6ff4
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-07 02:12:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end
-  if int % 5 == 0 # if the number int is divisible by 3
+  elsif int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
   end
 end
