c0a3adb98516b6100bf85d2a2584b01cc1eaf9ba
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-07 02:09:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
-def fizzbuzz(value)
-  x = value
-  if value % 3 == 0
+def fizzbuzz(int)
+  x = int
+  if int % 3 == 0
     x = "Fizz"
   end
-  if value % 5 == 0
+  if int % 5 == 0
     x = "Buzz"
   end
-  if value % 3 == 0 && value % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
     x = "FizzBuzz"
   end
-  
+
   puts "#{x}"
-  
+
 end
