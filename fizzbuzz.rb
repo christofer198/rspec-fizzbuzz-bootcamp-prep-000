c54c38cfd0042f1acd37ddf7208f49178c14611c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-07 02:08:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,15 @@
 def fizzbuzz(value)
+  x = value
+  if value % 3 == 0
+    x = "Fizz"
+  end
+  if value % 5 == 0
+    x = "Buzz"
+  end
+  if value % 3 == 0 && value % 5 == 0
+    x = "FizzBuzz"
+  end
   
-  return value
+  puts "#{x}"
+  
 end
